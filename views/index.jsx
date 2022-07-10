44c0f259aeef4481b09d9e08dc4986b14922e6b0
Jeff Truitt <jtruitt@gmail.com>
2022-07-10 01:02:57 -0700

    create and new routes complete

--- views/index.jsx
+++ views/index.jsx
@@ -1,16 +1,22 @@
+const { templateLiteral } = require('@babel/types')
 const React = require('react')
 const Default = require('./layouts/Default')
 
-function Index ({breads})  {
+function Index ({breads, title})  {
     return (
-      <Default>
+      <Default title={title}>
         <h2>Index Page</h2>
+        <div className="newButton">
+            <a href="/breads/new"><button>Add a new bread</button></a>
+        </div>
+
         {/* this is a JSX comment. */}
-        {/*<p>I have {breads[1].name} bread!</p>*/}
+        {/*<p>I have {breads[0].name} bread!</p>*/}
         <ul>
             {
               breads.map((bread, index)=>{
-                return(<li key={index}>
+                return(
+                    <li key={index}>
                     <a href={`/breads/${index}`}>
                     {bread.name}
                     </a>
