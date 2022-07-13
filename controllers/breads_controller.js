829aa257f43d7b722dcdb24118f232630b14aa7b
Jeff Truitt <jtruitt@gmail.com>
2022-07-13 02:35:40 -0700

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -22,7 +22,7 @@ breads.post('/', (req, res) => {
   } else {
     req.body.hasGluten = false
   }
-  //Bread.push(req.body)
+  Bread.push(req.body)
   console.log(req.body)
   res.redirect('/breads')
 })
@@ -33,6 +33,13 @@ breads.get('/new', (req, res) => {
   res.render('new')
 })
 
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
+})
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
@@ -51,6 +58,19 @@ breads.delete('/:indexArray', (req, res)=>{
   Bread.splice(req.params.indexArray, 1)
   res.status(303).redirect('/breads')
 })
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
+})
+
+
 
 
 module.exports = breads
